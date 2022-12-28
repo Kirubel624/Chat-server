82316172644236526ae6d4b227a8ae8691fd3273
kirubel <kirato747@gmail.com>
2022-12-28 14:39:33 +0300

    chat server upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const WebSocket = require('ws')
+const express = require('express')
+const moment = require('moment')
+const app = express()
+const port = 7878; 
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+var  webSockets = {}
+
+const wss = new WebSocket.Server({ port: 6060 }) //run websocket server with port 6060
+wss.on('connection', function (ws, req)  {
+    var userID = req.url.split('/').pop() //get userid from URL ip:6060/userid 
+    console.log(userID)
+    // str.slice(beginIndex, endIndex)
+    webSockets[userID] = ws //add new user to the connection list
+
+    console.log('User ' + userID + ' Connected ') 
+
+    ws.on('message', message => { //if there is any message
+        console.log(message);
+        var datastring = message.toString();
+        if(datastring.charAt(0) == "{"){
+            datastring = datastring.replace(/\'/g, '"');
+            var data = JSON.parse(datastring)
+            if(data.auth == "chatapphdfgjd34534hjdfk"){
+                if(data.cmd == 'send'){ 
+                    var boardws = webSockets[data.userid] //check if there is reciever connection
+                    if (boardws){
+                        var cdata = "{'cmd':'" + data.cmd + "','userid':'"+data.userid+"', 'msgtext':'"+data.msgtext+"'}";
+                        boardws.send(cdata); //send message to reciever
+                        ws.send(data.cmd + ":success");
+                    }else{
+                        console.log("No reciever user found.");
+                        ws.send(data.cmd + ":error");
+                    }
+                }else{
+                    console.log("No send command");
+                    ws.send(data.cmd + ":error");
+                }
+            }else{
+                console.log("App Authincation error");
+                ws.send(data.cmd + ":error");
+            }
+        }else{
+            console.log("Non JSON type data");
+            ws.send(data.cmd + ":error");
+        }
+    })
+
+    ws.on('close', function () {
+        var userID = req.url.substr(1)
+        delete webSockets[userID] //on connection close, remove reciver from connection list
+        console.log('User Disconnected: ' + userID)
+    })
+    
+    ws.send('connected'); //innitial connection return message
+})
